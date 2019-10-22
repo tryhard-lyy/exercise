6ecfd82d638b3b410fcc4c8d4cf452d21401f093
tryhard-lyy <55044070+tryhard-lyy@users.noreply.github.com>
2019-10-22 23:18:26 +0800

    Create zhishu.java

---
+++ daily/zhishu.java
@@ -0,0 +1,16 @@
+public class Test {
+    public static void main(String[] args) {
+        boolean flag;
+        for(int i = 2;i<=100;i++){
+            flag = true;
+            for(int j=2;j<=(i/2);j++){
+                if(i%j==0){
+                    flag = false;
+                }
+            }
+            if(flag == true){
+                System.out.println(i);
+            }
+        }
+    }
+}
