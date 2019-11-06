eaa570b8eaa5711527fb448e42b6a55b0006014f
tryhard-lyy <55044070+tryhard-lyy@users.noreply.github.com>
2019-11-06 17:38:14 +0800

    Create DateTime.java

---
+++ Util/DateTime.java
@@ -0,0 +1,26 @@
+package com.mmall.util;
+
+import org.apache.commons.lang3.StringUtils;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.util.Date;
+
+public class DateTimeUtil {
+    public static Date strToDate(String dateTimeStr,String formatStr){
+        DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern(formatStr);
+        DateTime dateTime = dateTimeFormatter.parseDateTime(dateTimeStr);
+
+        return dateTime.toDate();
+    }
+
+
+    public static String SateToStr(Date date,String formatStr){
+        if(date == null){
+            return StringUtils.EMPTY;
+        }
+        DateTime dateTime = new DateTime(date);
+        return dateTime.toString(formatStr);
+    }
+}
