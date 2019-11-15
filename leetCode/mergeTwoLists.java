b8022b15125afa5a2e0e2ad0bea71975df1e9cd9
tryhard-lyy <55044070+tryhard-lyy@users.noreply.github.com>
2019-11-15 17:51:26 +0800

    Create mergeTwoLists.java

---
+++ leetCode/mergeTwoLists.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null){
+            return l2;
+        }
+        if(l2 == null){
+            return l1;
+        }
+        
+        if(l1.val > l2.val){
+            l2.next = mergeTwoLists(l1,l2.next);
+            return l2;
+        }else{
+            l1.next = mergeTwoLists(l2,l1.next);
+            return l1;
+        }
+        
+    }
+}
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode temp = new ListNode(0);
+        
+        ListNode pre = temp;
+        
+        while(l1 != null && l2 != null){
+            if(l1.val > l2.val){
+                pre.next = l2;
+                l2 = l2.next;
+            }else{
+                pre.next = l1;
+                l1 = l1.next;
+            }
+            
+            pre = pre.next;
+        }
+        
+        pre.next = l1 == null ? l2:l1;
+        
+        return temp.next;
+    }
+}
