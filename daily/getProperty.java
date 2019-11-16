89bed8b7ce469fb58aead61807a5d498a7149d76
tryhard-lyy <55044070+tryhard-lyy@users.noreply.github.com>
2019-11-16 19:43:09 +0800

    Create getProperty.java

---
+++ daily/getProperty.java
@@ -0,0 +1,10 @@
+package baogao3.Test3;
+
+public class getProperty {
+    public static void main(String[] args) {
+        System.out.println("os.arch:"+System.getProperty("os.arch"));
+        System.out.println("os.name:"+System.getProperty("os.name"));
+        System.out.println("os.version:"+System.getProperty("os.version"));
+
+    }
+}
