840343a83eeca43a9260753dc985c73bf36781a8
tryhard-lyy <55044070+tryhard-lyy@users.noreply.github.com>
2019-11-16 19:44:12 +0800

    Create Interval.java

---
+++ daily/Interval.java
@@ -0,0 +1,12 @@
+package baogao3.Test3;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Interval {
+    public static void main(String[] args) {
+
+
+    }
+}
